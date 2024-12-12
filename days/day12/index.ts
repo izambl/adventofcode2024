4fb60c8ba21deaaaf0f0b6541f839ca9825e1f40
Ismael b <ismael@recursivamente.com>
2024-12-12 16:47:48 -0600

    Day 12 - Part 02

--- days/day12/index.ts
+++ days/day12/index.ts
@@ -5,7 +5,7 @@ import path from 'node:path';
 
 import { MakeTuple, readInput } from '../../common';
 
-const input = readInput(path.join(__dirname, 'inputTest'), '\n').map((row) => row.split(''));
+const input = readInput(path.join(__dirname, 'input01'), '\n').map((row) => row.split(''));
 
 type Position = [number, number];
 type Tile = {
@@ -50,41 +50,42 @@ function buildMap(rawMap: string[][]): TileMap {
 const map = buildMap(input);
 
 type Region = {
-  tiles: Map<Position, Tile>;
   tilesArray: Tile[];
   size: number;
+  plant: string;
+  map: TileMap;
 };
-const blocks: Region[] = [];
+const regions: Region[] = [];
 
 for (const [_, tile] of map.entries()) {
-  const isAlreadyInABlock = blocks.some((block) => block.tilesArray.includes(tile));
+  const isAlreadyInABlock = regions.some((block) => block.tilesArray.includes(tile));
 
   if (isAlreadyInABlock) continue;
 
-  const newBlock: Region = { tiles: new Map(), size: 0, tilesArray: [] };
+  const newRegion: Region = { map: new Map(), plant: tile.value, size: 0, tilesArray: [] };
 
   walkBlock(map, tile);
 
   function walkBlock(map: TileMap, startTile: Tile) {
-    newBlock.tiles.set(startTile.position, startTile);
-    newBlock.size += 1;
-    newBlock.tilesArray.push(startTile);
+    newRegion.size += 1;
+    newRegion.tilesArray.push(startTile);
+    newRegion.map.set(startTile.position, startTile);
 
     for (const direction of ['up', 'down', 'right', 'left'] as const) {
       const nextTile = startTile[direction];
 
       if (nextTile === null) continue;
       if (nextTile.value !== startTile.value) continue;
-      if (newBlock.tilesArray.includes(nextTile)) continue;
+      if (newRegion.tilesArray.includes(nextTile)) continue;
 
       walkBlock(map, nextTile);
     }
   }
 
-  blocks.push(newBlock);
+  regions.push(newRegion);
 }
 
-const part01 = blocks.reduce((total, block) => {
+const part01 = regions.reduce((total, block) => {
   const fences = block.tilesArray.reduce((fences, tile) => {
     let sum = 0;
     sum += tile?.up?.value !== tile.value ? 1 : 0;
@@ -98,5 +99,87 @@ const part01 = blocks.reduce((total, block) => {
   return total + fences * block.size;
 }, 0);
 
+let part02 = 0;
+for (const region of regions) {
+  const allXs = region.tilesArray.map((tile) => Number(tile.position[0]));
+  const allYs = region.tilesArray.map((tile) => Number(tile.position[1]));
+  const minX = Math.min(...allXs);
+  const maxX = Math.max(...allXs);
+  const minY = Math.min(...allYs);
+  const maxY = Math.max(...allYs);
+
+  // X scan
+  let xSides = 0;
+  for (let y = minY; y <= maxY; y++) {
+    let sideUpstart = false;
+    let sideDownStart = false;
+
+    for (let x = minX; x <= maxX; x++) {
+      const tile = region.map.get(T([x, y]));
+
+      if (!tile) {
+        if (sideUpstart) xSides++;
+        sideUpstart = false;
+        if (sideDownStart) xSides++;
+        sideDownStart = false;
+        continue;
+      }
+      if (tile.up?.value !== tile.value) {
+        sideUpstart = true;
+      } else {
+        if (sideUpstart) xSides++;
+        sideUpstart = false;
+      }
+      if (tile.down?.value !== tile.value) {
+        sideDownStart = true;
+      } else {
+        if (sideDownStart) xSides++;
+        sideDownStart = false;
+      }
+    }
+
+    if (sideUpstart) xSides++;
+    if (sideDownStart) xSides++;
+  }
+
+  // Y scan
+  let ySides = 0;
+  for (let x = minX; x <= maxX; x++) {
+    let sideLeftStart = false;
+    let sideRightStart = false;
+
+    for (let y = minY; y <= maxY; y++) {
+      const tile = region.map.get(T([x, y]));
+
+      if (!tile) {
+        if (sideLeftStart) ySides++;
+        sideLeftStart = false;
+        if (sideRightStart) ySides++;
+        sideRightStart = false;
+        continue;
+      }
+      if (tile.left?.value !== tile.value) {
+        sideLeftStart = true;
+      } else {
+        if (sideLeftStart) ySides++;
+        sideLeftStart = false;
+      }
+      if (tile.right?.value !== tile.value) {
+        sideRightStart = true;
+      } else {
+        if (sideRightStart) ySides++;
+        sideRightStart = false;
+      }
+    }
+
+    if (sideLeftStart) ySides++;
+    if (sideRightStart) ySides++;
+  }
+
+  console.log(region.plant, xSides, ySides, region.size);
+
+  part02 += (xSides + ySides) * region.size;
+}
+
 process.stdout.write(`Part 01: ${part01}\n`);
-process.stdout.write(`Part 02: ${2}\n`);
+process.stdout.write(`Part 02: ${part02}\n`);
