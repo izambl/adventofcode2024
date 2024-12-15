8f08f19c26abe7f9ad2e7c6d560284befe4d03ab
Ismael b <ismael@recursivamente.com>
2024-12-15 16:18:03 -0600

    Day 15 - part 02

--- days/day15/index.ts
+++ days/day15/index.ts
@@ -3,10 +3,11 @@
 
 import path from 'node:path';
 
+import { dir } from 'node:console';
 import { readInput } from '../../common';
 import { P, type Tile, type TileMap, buildMap2d, printMap } from '../../common/map-builder';
 
-const [rawMap, rawInstructions] = readInput(path.join(__dirname, 'inputTest'), '\n\n');
+const [rawMap, rawInstructions] = readInput(path.join(__dirname, 'input01'), '\n\n');
 
 const map = buildMap2d(rawMap.split('\n').map((row) => row.split('')));
 const instructions: Array<'<' | '>' | '^' | 'v'> = rawInstructions
@@ -77,11 +78,48 @@ const map2 = buildMap2d(
 
 let robot2 = [...map2.values()].find((tile) => tile.value === '@');
 
+function getMovableBlock(tile: Tile, direction: 'up' | 'down', movableBlock: Set<Tile>) {
+  if (!['[', ']', '@'].includes(tile.value)) return;
+
+  movableBlock.add(tile);
+
+  if (tile.value === '[') {
+    movableBlock.add(tile.right);
+    getMovableBlock(tile.right[direction], direction, movableBlock);
+  }
+  if (tile.value === ']') {
+    movableBlock.add(tile.left);
+    getMovableBlock(tile.left[direction], direction, movableBlock);
+  }
+
+  getMovableBlock(tile[direction], direction, movableBlock);
+}
+
+function moveBlock(block: Set<Tile>, direction: 'up' | 'down') {
+  const sortedBlock = [...block].sort((tileA, tileB) => {
+    return direction === 'down' ? tileB.position[1] - tileA.position[1] : tileA.position[1] - tileB.position[1];
+  });
+
+  for (const tile of sortedBlock) {
+    tile[direction].value = tile.value;
+    tile.value = '.';
+  }
+}
+
 function move2(tile: Tile, direction: 'right' | 'left' | 'up' | 'down') {
   if (tile[direction].value === '[' || tile[direction].value === ']') {
     if (direction === 'right' || direction === 'left') {
       move2(tile[direction], direction);
     } else {
+      const movableBlock = new Set<Tile>();
+      getMovableBlock(tile, direction, movableBlock);
+
+      // Not movable
+      if ([...movableBlock].some((t) => t[direction].value === '#')) {
+        return;
+      }
+      moveBlock(movableBlock, direction);
+      return;
     }
   }
 
@@ -100,10 +138,17 @@ printMap(map2);
 
 for (const direction of instructions) {
   const dir = DIR[direction];
+
   move2(robot2, dir);
   if (robot2[dir].value === '@') robot2 = robot2[dir];
 }
 
 printMap(map2);
 
-process.stdout.write(`Part 02: ${2}\n`);
+const part02 = [...map2.values()].reduce((total, tile) => {
+  if (tile.value !== '[') return total;
+
+  return tile.position[1] * 100 + total + tile.position[0];
+}, 0);
+
+process.stdout.write(`Part 02: ${part02}\n`);
